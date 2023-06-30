91ad858fc792ea4b3fc92ff09ade7f07ba92b49a
Ulyana <ushinkevich@mail.ru>
2023-07-01 00:41:15 +0400

    выводит автора задачи в список, оператор ??

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ import { renderLoginComponent } from "./components/login-component.js";
                 return `
                 <li class="task">
                     <p class="task-text">
-                    ${task.text}
+                    ${task.text} (Создал: ${task.user?.name ?? 'Неизвестно'})
                     <button data-id="${task.id}" class="button delete-button">Удалить</button>
                     </p>
                 </li>`;
