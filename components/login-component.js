4f9cd29a1f7a84333b1f5babd3bd8485d65abfe3
Ulyana <ushinkevich@mail.ru>
2023-07-01 00:20:28 +0400

    модуль «компонент формы входа»

---
+++ components/login-component.js
@@ -0,0 +1,26 @@
+export function renderLoginComponent({appEl, setToken, fetchTodosAndRender}) {
+    const appHtml = `
+                <h1>Список задач</h1>
+            <div class="form">
+                <h3 class="form-title">Форма входа</h3>
+                <div class="form-row">
+                Логин:
+                <input type="text" id="login-input" class="input"/>
+                <br>
+                Пароль:
+                <input type="text" id="login-input" class="input"/>
+                </div>
+                <br>
+                <button class="button" id="login-button">Войти</button>
+            </div>`;
+
+            
+            appEl.innerHTML = appHtml;
+
+            document.getElementById("login-button").addEventListener('click', () => {
+                
+                setToken("Bearer dgc0boasc8as6g5g5k5o5s5w606g39o3cc3e83ek3ck3b43k38o");
+
+                fetchTodosAndRender();
+            });
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { addTodo, deleteTodo, getTodos } from "./api.js";
+import { renderLoginComponent } from "./components/login-component.js";
 
     
     let tasks = [];
@@ -17,29 +18,14 @@ import { addTodo, deleteTodo, getTodos } from "./api.js";
     const renderApp = () => {
         const appEl = document.getElementById("app");
         if (!token) {
-            const appHtml = `
-                <h1>Список задач</h1>
-            <div class="form">
-                <h3 class="form-title">Форма входа</h3>
-                <div class="form-row">
-                Логин:
-                <input type="text" id="login-input" class="input"/>
-                <br>
-                Пароль:
-                <input type="text" id="login-input" class="input"/>
-                </div>
-                <br>
-                <button class="button" id="login-button">Войти</button>
-            </div>`
-
             
-            appEl.innerHTML = appHtml;
-
-            document.getElementById("login-button").addEventListener('click', () => {
-                token = "Bearer dgc0boasc8as6g5g5k5o5s5w606g39o3cc3e83ek3ck3b43k38o";
-                fetchTodosAndRender();
+            renderLoginComponent({
+                appEl, 
+                setToken: (newToken) => {
+                    token = newToken;
+                },
+                fetchTodosAndRender,
             });
-
             return;
         }
 
