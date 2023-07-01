f35c9567ee09c062085720fde6fe6082898c2025
Ulyana <ushinkevich@mail.ru>
2023-07-01 11:11:15 +0400

    заглушка регистрации

--- components/login-component.js
+++ components/login-component.js
@@ -30,34 +30,40 @@ export function renderLoginComponent({appEl, setToken, fetchTodosAndRender}) {
         appEl.innerHTML = appHtml;
 
         document.getElementById("login-button").addEventListener('click', () => {
-            const login = document.getElementById("login-input").value;
-            const password = document.getElementById("password-input").value;
 
-            if (!login) {
-                alert('Введите логин');
-                return;
-            }
-
-            if (!password) {
-                alert('Введите пароль');
-                return;
-            }
+            if (isLoginMode) {
+                const login = document.getElementById("login-input").value;
+                const password = document.getElementById("password-input").value;
+    
+                if (!login) {
+                    alert('Введите логин');
+                    return;
+                }
+    
+                if (!password) {
+                    alert('Введите пароль');
+                    return;
+                }
+                
+                setToken("Bearer dgc0boasc8as6g5g5k5o5s5w606g39o3cc3e83ek3ck3b43k38o");
+                    loginUser({
+                        login: login, 
+                        password: password,
+                    })
+                    .then((user) => {
+                        console.log(user);
+    
+                        setToken(`Bearer ${user.user.token}`);
+                        fetchTodosAndRender();
+                    })
+                    .catch(error => {
+                        // TODO: Выводить алерт красиво
+                        alert(error.message);
+                    });   
+            } else {
+                alert('Заглушка регистрации');
+            };
             
-            setToken("Bearer dgc0boasc8as6g5g5k5o5s5w606g39o3cc3e83ek3ck3b43k38o");
-                loginUser({
-                    login: login, 
-                    password: password,
-                })
-                .then((user) => {
-                    console.log(user);
-
-                    setToken(`Bearer ${user.user.token}`);
-                    fetchTodosAndRender();
-                })
-                .catch(error => {
-                    // TODO: Выводить алерт красиво
-                    alert(error.message);
-                });
 
         });
 
