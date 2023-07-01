28d18b96d2704c1a24c8186d53d811fad47ed70c
Ulyana <ushinkevich@mail.ru>
2023-07-01 10:53:32 +0400

    добавлена форма регистрации

--- components/login-component.js
+++ components/login-component.js
@@ -1,39 +1,49 @@
 import { loginUser } from "../api.js";
 
 export function renderLoginComponent({appEl, setToken, fetchTodosAndRender}) {
-    const appHtml = `
-                <h1>Список задач</h1>
-            <div class="form">
-                <h3 class="form-title">Форма входа</h3>
-                <div class="form-row">
-                Логин:
-                <input type="text" id="login-input" class="input"/>
-                <br>
-                Пароль:
-                <input type="password" id="password-input" class="input"/>
-                </div>
-                <br>
-                <button class="button" id="login-button">Войти</button>
-            </div>`;
+    let isLoginMode = false;
 
+    const renderForm = () => {
+
+        const appHtml = `
+        <h1>Список задач</h1>
+        <div class="form">
+        <h3 class="form-title">Форма ${isLoginMode ? 'входа' : 'регистрации'}</h3>
+        <div class="form-row">
+        ${isLoginMode ? '' : `Имя:
+        <input type="text" id="name-input" class="input"/>
+        <br>`}
+        
+        Логин:
+        <input type="text" id="login-input" class="input"/>
+        <br>
+        Пароль:
+        <input type="password" id="password-input" class="input"/>
+        </div>
+        <br>
+        <button class="button" id="login-button">${isLoginMode ? 'Войти' : 'Зарегистрироваться'}</button>
+        <br><br>
+        <button class="button" id="toggle-button">Перейти ${isLoginMode ? 'к регистрации' : 'ко входу'}</button>
+        </div>`;
+
+    
+        appEl.innerHTML = appHtml;
+
+        document.getElementById("login-button").addEventListener('click', () => {
+            const login = document.getElementById("login-input").value;
+            const password = document.getElementById("password-input").value;
+
+            if (!login) {
+                alert('Введите логин');
+                return;
+            }
+
+            if (!password) {
+                alert('Введите пароль');
+                return;
+            }
             
-            appEl.innerHTML = appHtml;
-
-            document.getElementById("login-button").addEventListener('click', () => {
-                const login = document.getElementById("login-input").value;
-                const password = document.getElementById("password-input").value;
-
-                if (!login) {
-                    alert('Введите логин');
-                    return;
-                }
-
-                if (!password) {
-                    alert('Введите пароль');
-                    return;
-                }
-                
-                setToken("Bearer dgc0boasc8as6g5g5k5o5s5w606g39o3cc3e83ek3ck3b43k38o");
+            setToken("Bearer dgc0boasc8as6g5g5k5o5s5w606g39o3cc3e83ek3ck3b43k38o");
                 loginUser({
                     login: login, 
                     password: password,
@@ -49,5 +59,14 @@ export function renderLoginComponent({appEl, setToken, fetchTodosAndRender}) {
                     alert(error.message);
                 });
 
-            });
+        });
+
+        document.getElementById("toggle-button").addEventListener('click', () => {
+            isLoginMode = !isLoginMode;
+            renderForm();
+        });
+    };
+
+    renderForm();
+
 }
